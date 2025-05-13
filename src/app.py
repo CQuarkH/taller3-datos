edf0ceaa9b649c8c29b6eccec7a5b744bf7928db
CQuark <currihuile@gmail.com>
2025-05-13 15:23:05 -0400

    feat: add more columns to extracted data

--- src/app.py
+++ src/app.py
@@ -23,7 +23,10 @@ def save_to_csv(features, output_path):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     fieldnames = [
         "id", "time", "latitude", "longitude", "depth",
-        "magnitude", "magType", "nst", "gap", "dmin", "rms", "place"
+        "magnitude", "magType", "nst", "gap", "dmin", "rms",
+        "felt", "cdi", "mmi", "sig", "tsunami",
+        "place", "status", "type", "net", "code", "ids",
+        "sources", "alert", "detail", "title"
     ]
 
     with open(output_path, mode="w", newline="", encoding="utf-8") as csvfile:
@@ -36,18 +39,32 @@ def save_to_csv(features, output_path):
             iso_time = datetime.utcfromtimestamp(props["time"] / 1000).isoformat() + "Z"
 
             writer.writerow({
-                "id": feat.get("id", ""),
+                "id": feat["id"],
                 "time": iso_time,
                 "latitude": coords[1],
                 "longitude": coords[0],
                 "depth": coords[2],
                 "magnitude": props.get("mag"),
-                "magType": props.get("magType", ""),
-                "nst": props.get("nst", ""),
-                "gap": props.get("gap", ""),
-                "dmin": props.get("dmin", ""),
-                "rms": props.get("rms", ""),
-                "place": props.get("place", "")
+                "magType": props.get("magType"),
+                "nst": props.get("nst"),
+                "gap": props.get("gap"),
+                "dmin": props.get("dmin"),
+                "rms": props.get("rms"),
+                "felt": props.get("felt"),
+                "cdi": props.get("cdi"),
+                "mmi": props.get("mmi"),
+                "sig": props.get("sig"),
+                "tsunami": props.get("tsunami"),
+                "place": props.get("place"),
+                "status": props.get("status"),
+                "type": props.get("type"),
+                "net": props.get("net"),
+                "code": props.get("code"),
+                "ids": props.get("ids"),
+                "sources": props.get("sources"),
+                "alert": props.get("alert"),
+                "detail": props.get("detail"),
+                "title": props.get("title")
             })
 
 def get_save_data():
